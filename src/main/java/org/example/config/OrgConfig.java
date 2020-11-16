d82d24c7ba149c5f2d3f576efb6519350b90afc5
ldstyle8 <875647760@qq.com>
2020-11-16 14:49:55 +0800

    first commit

--- src/main/java/org/example/config/OrgConfig.java
+++ src/main/java/org/example/config/OrgConfig.java
@@ -1,10 +1,35 @@
 package org.example.config;
 
+/*
+ * organization's configuration
+ * for example:
+ *   ORG1 = "org1"
+ *   ORG2_MSP = "Org2MSP"
+ *   ADMIN = "admin";
+ *   ADMIN_PASSWORD = "adminpw"
+ *   BASE_PATH = "crypto-config" + File.separator + "peerOrganizations" + File.separator + "org1.example.com" + File.separator + "users" + File.separator + "Admin@org1.example.com"
+ *   ADMIN_PK = ORG1_USR_BASE_PATH + File.separator + "keystore"
+ *   ADMIN_CERT = ORG1_USR_BASE_PATH + File.separator + "admincerts"
+ *  */
 public class OrgConfig {
     public String OrgName;
     public String OrgMsp;
+    //
     public String Admin;
     public String AdminPassWord;
+    // key & cert store
+    public String BasePath;
+    public String AdminPK;
+    public String AdminCert;
 
+    public OrgConfig(String orgName, String orgMsp, String admin, String adminPassWord, String basePath, String adminPK, String adminCert) {
+        this.Admin = admin;
+        this.OrgMsp = orgMsp;
+        this.OrgName = orgName;
+        this.AdminPassWord = adminPassWord;
+        this.BasePath = basePath;
+        this.AdminPK = adminPK;
+        this.AdminCert = adminCert;
+    }
 
 }
