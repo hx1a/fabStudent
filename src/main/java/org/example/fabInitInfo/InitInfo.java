f45f7e6f7f698f4248113a5d320592ea4eb72675
xiahan <xiahan@iie.ac.cn>
2020-11-16 17:57:21 +0800

    Change Peers to ArrayList

--- src/main/java/org/example/fabInitInfo/InitInfo.java
+++ src/main/java/org/example/fabInitInfo/InitInfo.java
@@ -5,22 +5,28 @@ import org.example.config.OrgConfig;
 import org.example.config.ChannelConfig;
 import org.example.config.ChainCodeConfig;
 
+import java.util.ArrayList;
+
 public class InitInfo {
-    protected int PeerNum;
+//    protected int PeerNum;
     protected int OrgNum;
     protected int ChannelNum;
     protected int ChaincodeNum;
 
-    protected PeerConfig[] Peers;
+    protected ArrayList Peers;
     protected OrgConfig[] Orgs;
     protected ChannelConfig[] Channels;
     protected ChainCodeConfig[] Chaincodes;
 
     public void setNum(int peerNum, int orgNum, int channelNum, int chaincodeNum) {
-        this.PeerNum = peerNum;
+//        this.PeerNum = peerNum;
         this.OrgNum = orgNum;
         this.ChaincodeNum = chaincodeNum;
         this.ChannelNum = channelNum;
+    }
+
+    public void addPeers(String name, String url){
+        Peers.add(new PeerConfig(name,url));
     }
 
 }
